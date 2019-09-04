55b100c864a438b83c0018f6668ebc1aef982995
Maks <mmasyush@gmail.com>
2019-09-04 19:25:11 +0300

    start commit

---
+++ includes/rtv.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rtv.h                                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmasyush <mmasyush@student.unit.ua>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/04 15:51:51 by mmasyush          #+#    #+#             */
+/*   Updated: 2019/09/04 15:51:51 by mmasyush         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef RTV_H
+# define RTV_H
+
+# include "../libft/libft.h"
+# include <math.h>
+# include <fcntl.h>
+#ifdef __APPLE__
+# include "../frameworks/SDL2.framework/Headers/SDL.h"
+#else
+# include <SDL.h>
+#endif
+
+
+#endif
